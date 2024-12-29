ec8cec601deaf60eff0e463fb66860e62f789b29
Soumya Ranjan <81520464+mohantysoumyaranjan741@users.noreply.github.com>
2024-12-29 18:19:15 +0000

    Commit

---
+++ PassingFuncAsArg.js
@@ -0,0 +1,10 @@
+function greetUser(name, callback) {
+    const message = `Hello, ${name}!`;
+    callback(message);
+}
+
+function logMessage(msg) {
+    console.log(msg);
+}
+
+greetUser("Alice", logMessage); // Output: Hello, Alice!

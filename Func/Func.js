288d106ea419637420cd23d17907dd2d2aabad06
Soumya Ranjan <81520464+mohantysoumyaranjan741@users.noreply.github.com>
2024-12-29 19:59:16 +0000

    Function Commit

---
+++ Func/Func.js
@@ -0,0 +1,4 @@
+function greet() {
+    console.log("Hello, World!");
+}
+greet(); // Output: Hello, World!
---
+++ Func/FuncAssignedToVar.js
@@ -0,0 +1,4 @@
+const greet = function() {
+    console.log("Hello, World!");
+};
+greet(); // Output: Hello, World!

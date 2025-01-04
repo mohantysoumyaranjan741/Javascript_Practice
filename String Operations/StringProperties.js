54d91e57af0741b26c876556b559d5a1a7271191
Soumya Ranjan <81520464+mohantysoumyaranjan741@users.noreply.github.com>
2025-01-04 09:24:58 +0000

    StringCommit

--- String Operations/StringProperties.js
+++ String Operations/StringProperties.js
@@ -8,3 +8,25 @@ console.log(parseFloat("123.45"))
 
 let str="Soumya"
 console.log(str.replace("S","H"))
+
+console.log("Hello" + " " + "World!");
+console.log("Hello".concat(" ", "World!"));
+
+console.log("hello,world".replace("hello","js"))
+//console.log("Hello,world!".replace("world", "JavaScript")); // Output: Hello, JavaScript!
+
+console.log("abc"==="abc")
+console.log("abc".localeCompare("Abc"))
+
+console.log("bccdfs".charAt(4))
+console.log("Sft".charCodeAt(2))
+console.log("abcd".substring(0,3))
+console.log(" hello".trimStart())
+console.log(" hello ".trim())
+console.log("hello ".trimEnd())
+console.log("hello".split("l"))
+console.log("hello,java".indexOf("hello"))
+console.log("hello,hello".lastIndexOf("hello"))
+console.log("hello,javascript".slice(-6))
+console.log("hello".toLowerCase())
+console.log("hello".toUpperCase())
